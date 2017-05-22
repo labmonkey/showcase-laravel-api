360913d6564254d4f68e9b6873266f3595d95ccd
Paweł Derehajło <derehajlo@gmail.com>
2017-05-22 21:33:21 +0100

    Added site controller with index action and it's view file

---
+++ app/Http/Controllers/SiteController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Vehicle;
+
+class SiteController extends Controller {
+
+	public function index() {
+		$vehicleModels = Vehicle::all();;
+
+		return view( 'index', [ 'vehicleModels' => $vehicleModels ] );
+	}
+
+}
